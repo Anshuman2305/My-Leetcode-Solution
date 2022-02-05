c66c0e0d711c801c347956e52e5efae308faa81a
Anshuman2305 <58540815+Anshuman2305@users.noreply.github.com>
2022-02-05 12:50:28 +0530

    Time: 137 ms (38.70%), Space: 46.6 MB (81.95%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums)
+{   
+        sort(nums.begin(), nums.end());
+        return unique(nums.begin(), nums.end()) != nums.end();
+}
+};
+
