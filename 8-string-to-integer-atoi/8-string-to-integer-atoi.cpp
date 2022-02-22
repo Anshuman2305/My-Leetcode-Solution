8e9a8c729615d57efe94b522fbee4d2ad484285b
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-02-22 19:09:27 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (13.28%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -19,8 +19,8 @@ public:
         }
         // Convert to integer and avoid overflow, avoid invalid input
         while (i < s.length() && s[i] >= '0' && s[i] <= '9') {
-            if (base > INT_MAX / 10 || 
-                (base == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
+            if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) 
+            {
                 return (sign == 1) ? INT_MAX : INT_MIN;
             }
             base = 10 * base + (s[i] - '0');
