e45313b0e9c9b81e00b8f0cdbe6561f7553455ac
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-02-22 09:32:49 +0530

    Time: 128 ms (53.74%), Space: 19.9 MB (96.41%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
-        set<vector<int> >rs;
-        sort(nums.begin(), nums.end());
-        int i, j, k;
-        int n = nums.size();
-        for (i = 0; i < n - 2; i++) {
-            j = i+1;
-            k = n-1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == 0) {
-                    vector<int> tmp(3);
-                    tmp[0] = nums[i];
-                    tmp[1] = nums[j];
-                    tmp[2] = nums[k];
-                    rs.insert(tmp);
-                    j++;
-                    k--;
-                } else if (sum > 0) {
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                }
-            }
-        }
-        vector<vector <int> >result (rs.begin(), rs.end());
-        return result;
+        vector<vector<int>> output;
+  sort(nums.begin(), nums.end());
+  for (int i = 0; i < nums.size(); ++i) {
+    // Never let i refer to the same value twice to avoid duplicates.
+    if (i != 0 && nums[i] == nums[i - 1]) continue;
+    int j = i + 1;
+    int k = nums.size() - 1;
+    while (j < k) {
+      if (nums[i] + nums[j] + nums[k] == 0) {
+        output.push_back({nums[i], nums[j], nums[k]});
+        ++j;
+        // Never let j refer to the same value twice (in an output) to avoid duplicates
+        while (j < k && nums[j] == nums[j-1]) ++j;
+      } else if (nums[i] + nums[j] + nums[k] < 0) {
+        ++j;
+      } else {
+        --k;
+      }
+    }
+  }
+  return output;
     }
 };
