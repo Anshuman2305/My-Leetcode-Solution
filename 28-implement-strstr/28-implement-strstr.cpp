757230dfdc20f0f37f52e43e44da6b4f90258c2e
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-02-23 11:54:39 +0530

    Time: 209 ms (44.39%), Space: 368.7 MB (12.39%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int count=0;
-        if(needle.empty()) return 0;
-        
-        for(int i=0;i<haystack.length();i++)
-        {
-            if(haystack[i]==needle[0])
-            {
-                int j;
-                for(j=0;j<needle.length();j++)
-                {
-                    if(haystack[i+j]!=needle[j]) break;
-                }
-                if(j==needle.length()) return i;
-            }
+        int len = needle.size();
+        if (haystack.size() < len) return -1;
+        for (int idx=0; idx <= haystack.size()- len; idx++){
+            if (string (haystack.begin()+idx, haystack.begin()+idx+len) == needle) return idx;
         }
         return -1;
     }
