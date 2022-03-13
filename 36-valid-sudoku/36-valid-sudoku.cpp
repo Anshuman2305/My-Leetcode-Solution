d6fe824b024fd9b75fade65633c9c05d8d986956
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-03-13 11:44:26 +0530

    Time: 39 ms (37.06%), Space: 18.1 MB (74.90%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        int used1[9][9] = {(0,0)}, used2[9][9] = {(0,0)}, used3[9][9] = {(0,0)}; //putting all element's value 0 
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				if (board[i][j] != '.') {
+					int num = board[i][j] - '1';  //for num value 0 to 8 we are doing -'1' //we are converting char to int
+					int k = i / 3 * 3 + j / 3;
+					if (used1[i][num] == 1 || used2[j][num] == 1 || used3[k][num] == 1)
+						return false;
+					used1[i][num] = used2[j][num] = used3[k][num] = 1;
+				}
+			}
+		}
+		return true;	
+    }
+};
