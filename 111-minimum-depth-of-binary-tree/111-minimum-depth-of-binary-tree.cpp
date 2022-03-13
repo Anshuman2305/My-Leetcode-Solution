165797feea18364d6e3d4708141af64c7ff16398
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-03-13 19:45:19 +0530

    Time: 531 ms (5.32%), Space: 144.9 MB (25.51%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -12,25 +12,11 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-    if(root == NULL)
-        return 0;
-    queue < pair <TreeNode* , int> > q;
-    q.push({root , 1});
-    TreeNode* frontNode;
-    int depth;
-    while(!q.empty())
-    {
-        frontNode = q.front().first;
-        depth = q.front().second;
-        q.pop();
-        if(frontNode->left == NULL && frontNode->right == NULL)
-            return depth;
-        if(frontNode->left != NULL)
-            q.push({frontNode->left , depth + 1});
-        if(frontNode->right != NULL)
-            q.push({frontNode->right , depth + 1});
-    }
-    return -1;
-
+        if(root == NULL) return 0;
+        if(root->left != NULL && root->right != NULL) //when both the side of the root exist
+        
+             return 1 + min(minDepth(root->left), minDepth(root->right));  //adding +1 on each recursive calling
+        
+        return 1 + max(minDepth(root->left), minDepth(root->right)); //when only one side of the root exist
     }
 };
