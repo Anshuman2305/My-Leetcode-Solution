a593cd21746a9f099d18467a262d56f5ee80d987
Anshuman Mohanty <58540815+Anshuman2305@users.noreply.github.com>
2022-03-14 16:30:01 +0530

    Time: 86 ms (87.61%), Space: 10.3 MB (79.82%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -8,9 +8,10 @@ public:
         }
         
         if(sum%2 != 0) return false;
+        
         vector<int> dp(sum + 1, 0);
         dp[0] = 1;
-        for (const int num : nums) {
+        for (auto num : nums) {
             for (int i = sum; i >= 0; --i)
                 if (dp[i]) dp[i + num] = 1;
             if (dp[sum / 2]) return true;
